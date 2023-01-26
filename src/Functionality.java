93f806b0656b597c8ca90733fb6fd678c72b4d69
lbluett <85853561+lbluett@users.noreply.github.com>
2023-01-26 17:57:28 +1000

    Initial commit

---
+++ src/Functionality.java
@@ -0,0 +1,10 @@
+public class Functionality {
+    // Here we will add the variables that the user has set
+    // Write a method that updates the variable when element is changed on gui? I suppose
+    // this will be called on the gui side when var is changed if there is not a simpler way.
+
+
+    public void typeKeys(String input) {
+
+    }
+}
