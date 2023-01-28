ce6cf8c07499275a5e2a5dc5251751d6c26bd5da
lbluett <85853561+lbluett@users.noreply.github.com>
2023-01-28 17:17:36 +1000

    Testing w/ Robot

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,31 @@
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         // Run threads when program is running.
+        String text = "I love A";
+        StringSelection stringSelection = new StringSelection(text);
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        clipboard.setContents(stringSelection, stringSelection);
+
+        Robot robot = null;
+        try {
+            robot = new Robot();
+        } catch (AWTException e) {
+            throw new RuntimeException(e); // TODO: tell user their device isn't compatible
+        }
+
+        robot.keyPress(KeyEvent.VK_CONTROL);
+        robot.keyPress(KeyEvent.VK_V);
+        robot.keyRelease(KeyEvent.VK_V);
+        robot.keyRelease(KeyEvent.VK_CONTROL);
         System.out.println("Hello world!");
+
+
     }
 }
+
+// https://unicode-org.github.io/icu-docs/apidoc/released/icu4j/
